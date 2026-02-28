d5a9d44044ef60fa59bc6eae59e46c6f37ad31a0
Bisan <bisansh3@gmail.com>
2026-03-01 00:08:34 +0300

    tested for multiple thread counts, added the advanced version (stop when changes converge below epsilon)

--- Project.cpp
+++ Project.cpp
@@ -83,17 +83,100 @@ void heat_par()
     outFilePar.close();
 }
 
+
+
+
+void heat_par(int x)
+{
+	omp_set_num_threads(x);
+    
+    for (int i = 1; i <= T; i++)
+    {
+#pragma omp parallel for
+        for (int j = 1; j < N - 1; j++)
+        {
+            uNext[j] = 0.5 * (u[j - 1] + u[j + 1]);
+        }
+
+        double* temp = u;
+        u = uNext;
+        uNext = temp;
+
+        
+    }
+
+}
+
+void heat_par_conv()
+{
+	double ep = 1e-4 ;
+	double diff;
+
+    do {
+        diff = 0;
+#pragma omp parallel for reduction (max:diff)
+        for (int j = 1; j < N - 1; j++)
+        {
+
+            uNext[j] = 0.5 * (u[j - 1] + u[j + 1]);
+			
+            curr_diff = abs(uNext[j] - u[j]);
+            if (curr_diff > diff)
+				diff = curr_diff;
+        }
+        
+        double* temp = u;
+        u = uNext;
+        uNext = temp;
+
+        while (diff > ep);
+    }
+
+}
+
+
+
+void test_num_threads(double seq_time) {
+
+    for (i = 8; i >= 1; i / 2) {
+		initialize();
+		double t1 = omp_get_wtime();
+		heat_par(i);
+		double t2 = omp_get_wtime();
+		cout << "Parallel time with " << i << " threads = " << t2 - t1 << "\n";
+		cout << "Speedup " << seq_time / (t2 - t1) << "\n";
+		cout << "Efficiency " << (seq_time / (t2 - t1)) / i << "\n";
+    }
+
+}
+
+
+
 int main()
 {
     initialize();
     double t1 = omp_get_wtime();
     heat_seq();
     double t2 = omp_get_wtime();
-    cout << "Seq time = " << t2 - t1 << "\n";
+    cout << "Sequential time = " << t2 - t1 << "\n";
     initialize();
     double t3 = omp_get_wtime();
     heat_par();
     double t4 = omp_get_wtime();
-    cout << "Par time = " << t4 - t3 << "\n";
+    cout << "Parallel time = " << t4 - t3 << "\n";
+	double speedup = (t2 - t1) / (t4 - t3);
+	cout << "Speedup = " << speedup << "\n";
+	cout << "Efficiency = " << speedup / omp_get_num_threads() << "\n";
+
+	test_num_threads(t2-t1);
+
+    cout << "advanced version " << endl;
+	initialize();
+	double t5 = omp_get_wtime();
+    heat_par_conv();
+    double t6 = omp_get_wtime();
+    cout << "parallel time advanced version: " << t6 - t5 << endl;
+
+
     return 0;
 }
